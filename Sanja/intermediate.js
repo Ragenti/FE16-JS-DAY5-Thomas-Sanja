576265d3fba679987e9788a9f943b715e92966e4
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-06-02 14:25:54 +0200

    inter finished

--- Sanja/intermediate.js
+++ Sanja/intermediate.js
@@ -6,17 +6,7 @@ printTable = () => {
     tableEmployers.innerHTML = "";
     employersList.forEach((employer) => {
         tableEmployers.innerHTML += `<table class="table table-dark table-hover table-employers">
-<thead>
-    <tr>
-        <th scope="col">#</th>
-        <th scope="col">Personal Id</th>
-        <th scope="col">First Name</th>
-        <th scope="col">Last Name</th>
-        <th scope="col">Email</th>
-        <th scope="col">Job Title</th>
-        <th scope="col">Salary</th>
-    </tr>
-</thead>
+
 <tbody>
     <tr>
         <th scope="row"></th>
@@ -28,7 +18,8 @@ printTable = () => {
         <td>${employer.salary}</td>
     </tr>
 </tbody>
-</table>`
+</table>`;
 
-    })
-}
+    });
+};
+printTable();
